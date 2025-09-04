b94179c501b96646b585f0d8e9af917dc794dac0
GregProuty <gprout009@gmail.com>
2025-09-04 12:24:23 -0700

    change rpc

--- utils/config.ts
+++ utils/config.ts
@@ -28,7 +28,7 @@ const getConfig = (env: string = defaultNetwork) => {
     case "mainnet":
       return {
         networkId: "mainnet",
-        nodeUrl: "rpc.near.org",
+        nodeUrl: "https://rpc.near.org",
         walletUrl: "https://wallet.near.org",
         helperUrl: "https://helper.mainnet.near.org",
         explorerUrl: "https://explorer.mainnet.near.org",
